6cec98fec41c3e751ed71fd523e660cab61f7bf4
apanizo <adolfo.panizo>
2018-05-29 10:39:10 +0200

    Injecting Web3 once

--- src/web3/index.js
+++ src/web3/index.js
@@ -1,5 +1,25 @@
 import Web3 from 'web3'
 
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://rinkeby.infura.io:443`));
+const initWeb3 = () => {
+  const web3 = new Web3(window.web3.currentProvider)
+
+  return web3 
+}
+
+const ensureOnce = (fn) => {
+  let executed = false
+  let response
+
+  return (...args) => {
+    if (executed) { return response }
+
+    executed = true
+    response = fn.apply(undefined, args)
+
+    return response
+  }
+}
+
+const web3 = ensureOnce(initWeb3) 
 
 export default web3
